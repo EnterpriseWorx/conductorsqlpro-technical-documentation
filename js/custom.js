17a8ff112e4d70b8e12335a0537e099e73ba3500
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2022-10-14 11:47:09 +0200

    Deployed ae0deee with MkDocs version: 1.3.0

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
-	//wrap all tables in a div with table-responsive class and make them striped
-  $('table').wrap("<div class='table-responsive'></div>");
-  $('table').addClass("table-striped");
-      
+  //wrap all tables in a div with table-responsive class and make them striped
+  $("table").wrap("<div class='table-responsive'></div>");
+  $("table").addClass("table-striped");
+
   //make images responsive and make images work with lightbox
-  $('.wm-page-content').find('img').each(function() {
-    //add lightbox image support
-    var src = $( this ).attr('src');
-    var lightboxLink = "<a class='fancybox' href='"+src+"'></a>";
-    $( this ).wrap(lightboxLink);
-    
-    $(".fancybox").fancybox();
-  });
+  $(".wm-page-content")
+    .find("img")
+    .each(function () {
+      //add lightbox image support
+      var src = $(this).attr("src");
+      var lightboxLink = "<a class='fancybox' href='" + src + "'></a>";
+      $(this).wrap(lightboxLink);
+
+      $(".fancybox").fancybox();
+    });
 });
