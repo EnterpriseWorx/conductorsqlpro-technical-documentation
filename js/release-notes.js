26b60a17b297401d1efa1af40f084f62cb52e3b3
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2026-07-27 10:54:08 +0200

    Deployed 6bc6cb4 with ProperDocs version: 1.6.7

--- js/release-notes.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('.section').find('li').each(function() {
-    var text = this.textContent;
-    text = text.replace(/HUMMDB-\d+\s*/g, '')
-    this.textContent = text;
-  });
-});
